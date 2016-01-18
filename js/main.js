5dd0776eb45e368d6f8943a14cfacbcbaa2c4070
nikrb <nik_rb@yahoo.com>
2016-01-18 20:10:44 +0000

    svg display inner (seconds) and outer(minutes) circles/doughnuts

--- js/main.js
+++ js/main.js
@@ -1,30 +1,42 @@
 // change the browser tab text
 // document.title = 'New title';
+// set countdown timer time
+/**
 
-$(document).ready( function(){
-  /*
   x = R*cos(theta) and y = R*sin(theta)
-  */
-  // 0 <= a <= 60
-  function toRad( a){
-    return a*(Math.PI/180);
-  }
-
-  var cx = 100;
-  var cy = 100;
-  var r = 70;
-  var pathstring = "100,100 ";
-  var x,y;
-  for( var i = -90; i < 270; i+= 45){
-    x = cx + r * Math.cos( toRad(i));
-    y = cy + r* Math.sin( toRad(i));
-    pathstring += " "+x+","+y;
-  }
-  x = cx + r * Math.cos( toRad( 240));
-  y = cy + r* Math.sin( toRad( 240));
-  pathstring += " "+x+","+y+" 100,100";
-  $('#pathClip').attr( "points", pathstring);
-  $('#pathGhost').attr( "points", pathstring);
-  $('.btn').click( function( e){
-  });
+ second timer:
+  countdown from start (25) to 0
+  angle goes from 270 to -90
+  calc
+    360/25 = delta
+    
+*/
+$(document).ready( function(){
+    function toRad( a){
+        return a*(Math.PI/180);
+    }
+    function drawFace( id, cx, cy, radius, angle){
+        console.log( "@drawFace args:", arguments);
+        var pathstring = "100,100 ";
+        var x,y;
+        for( var i = angle; i > -90 ; i -= 45){
+            x = cx + radius * Math.cos( toRad(i));
+            y = cy + radius* Math.sin( toRad(i));
+            pathstring += " "+x+","+y;
+        }
+        x = cx + radius * Math.cos( toRad( -90));
+        y = cy + radius* Math.sin( toRad( -90));
+        pathstring += " "+x+","+y+" 100,100";
+        $('#'+id+">polyline").attr( "points", pathstring);
+        // $('#pathGhost').attr( "points", pathstring);
+    }
+    var sf = $( '#seconds_face');
+    drawFace( 'seconds_clip_path', parseFloat( sf.attr( "cx")), parseFloat( sf.attr( "cy")),
+                                    parseFloat( sf.attr( "r"))+20, 240);
+    var mf = $('#minutes_face');
+    drawFace( 'minutes_clip_path', parseFloat( mf.attr( 'cx')), parseFloat( mf.attr( 'cy')),
+                                    parseFloat( sf.attr( 'r'))+30, 220);
+    // drawFace( 'minutes_clip_path', 90, 140);
+    $('.btn').click( function( e){
+    });
 });
